1efc2d066bed6e71e0d6a670e65e015f457d5214
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-08-07 12:49:44 -0500

    interfaz

--- Controladores/MainController.java
+++ Controladores/MainController.java
@@ -1,5 +1,34 @@
 package Controladores;
 
-public class MainController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import Vistas.GUIPlagio;
+
+public class MainController implements ListSelectionListener, ActionListener{
+
+    private GUIPlagio interfaz;
+
+    public MainController() {
+        this.interfaz = new GUIPlagio();
+        this.interfaz.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new MainController();
+    }
     
 }
--- Vistas/GUIPlagio.java
+++ Vistas/GUIPlagio.java
@@ -16,9 +16,11 @@ import javax.swing.table.DefaultTableModel;
 public class GUIPlagio extends JFrame {
     
     public JTextArea texto;
+    public JTextArea textShow;
     public JButton addTextDb;
     public JButton addTextDbFile;
     public JButton removeFile;
+    public JButton diselect;
 
     public JButton verify;
     public JButton clearText;
@@ -33,7 +35,6 @@ public class GUIPlagio extends JFrame {
 
         this.initTabbed();
 
-        this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
@@ -52,7 +53,7 @@ public class GUIPlagio extends JFrame {
         textoPanel.setBorder(new TitledBorder("TextoPanel a analizar"));
         textoPanel.setBounds(10, 10, 380, 520);
         
-        texto = new JTextArea(30, 20);
+        texto = new JTextArea();
         texto.setLineWrap(true);
         texto.setWrapStyleWord(true);
         texto.setMargin(new Insets(10, 10, 10, 10));
@@ -78,11 +79,28 @@ public class GUIPlagio extends JFrame {
         secundario.setLayout(null);
         
         JPanel actions2 = new JPanel();
+        actions2.setLayout(null);
         actions2.setBorder(new TitledBorder("Acciones"));
         secundario.add(actions2);
         actions2.setBounds(10, 10, 170, 200);
 
         this.addTextDb = new JButton("Añadir texto");
+        this.addTextDbFile = new JButton("Añadir desde archivo");
+        this.removeFile = new JButton("Eliminar texto");
+        this.diselect = new JButton("Deseleccionar");
+
+        this.diselect.setEnabled(false);
+        this.removeFile.setEnabled(false);
+
+        actions2.add(addTextDb);
+        actions2.add(addTextDbFile);
+        actions2.add(removeFile);
+        actions2.add(diselect);
+
+        addTextDb.setBounds(10, 25, 150, 35);
+        addTextDbFile.setBounds(10, 65, 150, 35);
+        removeFile.setBounds(10, 105, 150, 35);
+        diselect.setBounds(10, 145, 150, 35);
 
         JPanel tablePanel = new JPanel();
         tablePanel.setLayout(null);
@@ -94,6 +112,28 @@ public class GUIPlagio extends JFrame {
         modelTable.setColumnIdentifiers(new String[]{"ID","Nombre de archivo", "Direccion"});
         JTable tabla = new JTable(modelTable);
         JScrollPane scrollTable = new JScrollPane(tabla);
+
+        tabla.getSelectionModel().addListSelectionListener(e -> {
+            int viewRow = tabla.getSelectedRow();
+            if (!e.getValueIsAdjusting() && viewRow != -1) {
+                this.diselect.setEnabled(true);
+                this.removeFile.setEnabled(true);
+            }
+        });
+
+        JPanel textPanelShow = new JPanel();
+        textPanelShow.setLayout(null);
+        textPanelShow.setBorder(new TitledBorder("Vista de texto"));
+        secundario.add(textPanelShow);
+        textPanelShow.setBounds(10, 210, 560, 320);
+
+        this.textShow = new JTextArea();
+        this.textShow.setMargin(new Insets(10, 10, 10, 10));
+        this.textShow.setLineWrap(true);
+        this.textShow.setWrapStyleWord(true);
+        JScrollPane scrollTextShow = new JScrollPane(this.textShow);
+        textPanelShow.add(scrollTextShow);
+        scrollTextShow.setBounds(10, 25, 540, 285);
         
         tablePanel.add(scrollTable);
         scrollTable.setBounds(10, 20, 360, 170);
@@ -115,9 +155,5 @@ public class GUIPlagio extends JFrame {
 
         panel.add(this.verify);
         panel.add(this.clearText);
-    }
-
-    public static void main(String[] args) {
-        new GUIPlagio();
     }
 }
