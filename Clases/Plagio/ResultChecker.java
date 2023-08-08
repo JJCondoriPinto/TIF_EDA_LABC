ace89db8cce31f8e63e6c35da20a329d46eae901
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-08-08 09:37:31 -0500

    clase para las coincidencias

--- Clases/Plagio/ResultChecker.java
+++ Clases/Plagio/ResultChecker.java
@@ -1,5 +1,25 @@
 package Clases.Plagio;
+import Clases.Listas.*;
 
 public class ResultChecker {
-    
+    private String originalText;
+    private LinkedList<Match> matches;
+
+    public ResultChecker(String originalText) {
+        this.originalText = originalText;
+        matches = new LinkedList<>(null);
+    }
+
+    public void addMatch(int startIndex, int endIndex) {
+        matches.add(new Match(startIndex, endIndex));
+    }
+
+    public String getOriginalText() {
+        return originalText;
+    }
+
+    public LinkedList<Match> getMatches() {
+        return matches;
+    }
 }
+
