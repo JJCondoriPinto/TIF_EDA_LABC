0f0bd95ecd5f352ce182d82d3167a1f14ec2f141
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-08-08 08:51:27 -0500

    adecuando clase trie con indice de posicion

--- Clases/Trie/Trie.java
+++ Clases/Trie/Trie.java
@@ -2,31 +2,43 @@ package Clases.Trie;
 
 public class Trie {
     private TrieNode root;
+    private int currentIndex; // Índice actual para asignar a las palabras
 
     public Trie() {
         root = new TrieNode();
+        currentIndex = 0;
     }
 
-    public void insert(String word) {
+    public void insertText(String text) {
+        String[] words = text.split("\\s+");
+
+        for (String word : words) {
+            insert(word.toLowerCase(), currentIndex);
+            currentIndex++;
+        }
+    }
+
+    public void insert(String word, int index) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
-            int index = c - 'a';  // Obtener el índice correspondiente al carácter en el arreglo
-            if (node.children[index] == null) {
-                node.children[index] = new TrieNode();
+            int charIndex = c - 'a';
+            if (node.children[charIndex] == null) {
+                node.children[charIndex] = new TrieNode();
             }
-            node = node.children[index];
+            node = node.children[charIndex];
         }
         node.isEndOfWord = true;
+        node.wordIndex = index;
     }
 
     public boolean search(String word) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
-            int index = c - 'a';
-            if (node.children[index] == null) {
+            int charIndex = c - 'a';
+            if (node.children[charIndex] == null) {
                 return false;
             }
-            node = node.children[index];
+            node = node.children[charIndex];
         }
         return node.isEndOfWord;
     }
@@ -34,11 +46,11 @@ public class Trie {
     public boolean startsWith(String prefix) {
         TrieNode node = root;
         for (char c : prefix.toCharArray()) {
-            int index = c - 'a';
-            if (node.children[index] == null) {
+            int charIndex = c - 'a';
+            if (node.children[charIndex] == null) {
                 return false;
             }
-            node = node.children[index];
+            node = node.children[charIndex];
         }
         return true;
     }
